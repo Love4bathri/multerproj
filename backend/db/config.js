37ec52a125226aeb208ac7cbd35261fcf1a34d99
riya4deshmukh <riyadeshmukh126@gmail.com>
2025-08-04 00:35:32 +0530

    api

--- backend/db/config.js
+++ backend/db/config.js
@@ -1,5 +1,11 @@
-const mongoose = require("mongoose")
-require('dotenv').config()
-module.exports =mongoose.connect("mongodb://localhost:27017/school")
- .then(() => console.log("MongoDB connected"))
- .catch(err => console.log("MongoDB connection error:", err));
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log("✅ MongoDB Connected");
+}).catch((err) => {
+  console.log("❌ MongoDB connection error:", err);
+});
